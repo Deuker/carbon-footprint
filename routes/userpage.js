9e710060ff31e4ae6f35b3d0a2c59a02c9648166
Victoria Kiseleva <111victoria.toria@gmail.com>
2020-04-28 12:34:11 +0200

    working logic for getting iata codes from city names.

--- routes/userpage.js
+++ routes/userpage.js
@@ -4,40 +4,8 @@ const router = express.Router();
 const Airport = require("../models/Airport");
 // const ensureLogin = require("connect-ensure-login");
 
-// router.get("/userpage", ensureLogin.ensureLoggedIn(), (req, res) => {
-//   res.render("userpage/userpage", { user: req.user });
-// });
-router.get("/userpage", (req, res, next) => {
-  res.render("userpage/userpage");
-});
-
 router.get("/searchresults", (req, res, next) => {
   res.render("userpage/searchresults");
 });
 
-router.post("/userpage", (req, res) => {
-  console.log(req.body);
-  Airport.find({ municipality: req.body.pointA })
-    .then((data) => {
-      data = data.filter((el) => el && el.iata_code);
-      console.log(data[0].iata_code);
-      console.log(data);
-      res.render("userpage/searchresults");
-    })
-    .catch((err) =>
-      console.log("The error while searching artists occurred: ", err)
-    );
-  Airport.find({ municipality: req.body.pointB })
-    .then((data) => {
-      data = data.filter((el) => el && el.iata_code);
-      console.log(data[0].iata_code);
-      console.log(data);
-      res.render("userpage/searchresults");
-    })
-    .catch((err) =>
-      console.log("The error while searching artists occurred: ", err)
-    );
-});
-
 module.exports = router;
-
